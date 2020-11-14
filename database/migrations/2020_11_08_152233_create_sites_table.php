eae475b3f43c4a0a0cf7095047ba92986d940ce4
Tim <bkkkd@163.com>
2020-11-14 21:29:36 +0800

    init

---
+++ database/migrations/2020_11_08_152233_create_sites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('main_host')->unique();
+            $table->text('alias_host');
+            $table->bigInteger('ssl_id');
+            $table->string('proxy_scheme');
+            $table->string('proxy_host');
+            $table->string('proxy_port');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
---
+++ database/migrations/2020_11_11_124205_create_ssls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSslsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ssls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('key');
+            $table->text('pub');
+            $table->text('clain');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ssls');
+    }
+}
